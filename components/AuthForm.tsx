12770274ee3f9823f9bdc710733ba13ab41fe659
Luan <luanitachi@hotmail.com>
2025-04-23 16:08:20 -0300

    home page. nav and card interviews

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -62,7 +62,7 @@ export default function AuthForm({type}: {type: FormType}) {
           <div className="flex flex-col gap-6 card py-14 px-10">
             <div className="flex flex-row gap-2 justify-center">
               <Image src="/logo.svg" alt="logo" height={32} width={38}/>
-              <h2 className="text-primary-100">PrepWise</h2>
+              <h2 className="text-primary-100">AlphaInsight</h2>
             </div>
 
             <h3>Practice job interview with AI</h3>
