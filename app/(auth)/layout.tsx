ecd26d34c3d9c6ec6d215f9808546554908e5b69
Luan <luanitachi@hotmail.com>
2025-04-27 11:53:49 -0300

    implement auth and storage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
+import { isAuthenticated } from "@/lib/actions/auth.actions";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-export default function AuthLayout({ children }: { children: ReactNode}) {
+export default async function AuthLayout({ children }: { children: ReactNode}) {
+    const isUserAuthenticated = await isAuthenticated()
+    
+        if (isUserAuthenticated) redirect('/')
     return (
         <div className="auth-layout">
             {children}
