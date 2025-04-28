c6440c5da99da4634d62f4db907c366df5cf837f
Luan <luanitachi@hotmail.com>
2025-04-28 16:36:49 -0300

    implement interview generation page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,10 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+export default function Page() {
+    return (
+       <>
+        <Agent userName='you' userId='user1' type='generate'/>
+       </>
+    )
+}
