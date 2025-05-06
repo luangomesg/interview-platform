fe41275cfeeb2100237efbd45e0d4f58de6a3588
Luan <luanitachi@hotmail.com>
2025-05-06 14:58:55 -0300

    vapi agent(test)

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,10 +1,13 @@
 import Agent from '@/components/Agent'
+import { getCurrentUser } from '@/lib/actions/auth.actions'
 import React from 'react'
 
-export default function Page() {
+export default async function Page() {
+
+    const user = await getCurrentUser()
     return (
        <>
-        <Agent userName='you' userId='user1' type='generate'/>
+        <Agent userName={user?.name} userId={user?.id} type='generate'/>
        </>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="dark" >
+    <html lang="en" className="dark" >
       <body
         className={`${monaSans.className} antialiased pattern `}
       >
